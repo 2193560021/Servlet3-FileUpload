75380a4c7d5add8d91be9ecd3b505599e40d5073
2193560021 <62364555+2193560021@users.noreply.github.com>
2021-12-01 12:29:18 +0800

    作业提交

---
+++ src/cn/edu/xupt/upload/UploadServlet.java
@@ -0,0 +1,99 @@
+package cn.edu.xupt.upload;
+
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+@MultipartConfig(maxFileSize = 1024 * 1024 * 10)
+@WebServlet("/UploadServlet")
+public class UploadServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+    private static final MultipartConfig config;
+
+    // 得到注解信息
+    static
+    {
+        config = UploadServlet.class.getAnnotation(MultipartConfig.class);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+//        Play play = new Play();
+        Part part;
+        try
+        {
+
+            ServletFileUpload upload = new ServletFileUpload();
+            request.setCharacterEncoding("UTF-8");
+            upload.setHeaderEncoding("UTF-8");
+    //然后对获取的FileItem执行如下转码方式
+            // 接收文本
+            String ch6 = "ch6";
+            int ID = Integer.parseInt(request.getParameter("id"));
+            // 接收图片:图片封装在part对象中
+            part = request.getPart("homework");
+            String fileName = getFileName(part);
+//            play.setPlayImage(fileName);
+            // 保存图片
+            part.write(getServletContext().getRealPath("/doc/") + ID + "-ch6.pdf");
+            System.out.println(fileName);
+            System.out.println(part.getSize());
+
+            // 带着play对象转发到result.java页
+//            request.setAttribute("play", play);
+            request.getRequestDispatcher("lxILoveYou.html").forward(request, response);
+        }
+        catch(Exception e)
+        {
+            if(config.maxRequestSize() == -1L || config.maxFileSize() == -1L)
+            {
+                System.out.println("上传文件过大!");
+            }
+            request.setAttribute("desc", "上传文件过大(限制10M)，或存在异常!");
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }
+
+    }
+
+    /**
+     * 从Part的Header信息中提取上传文件的文件名
+     * @param part
+     * @return 上传文件的文件名，如果如果没有则返回null
+     */
+    private String getFileName(Part part)
+    {
+        // 获取header信息中的content-disposition，如果为文件，则可以从其中提取出文件名
+        // IE下文件名带路径，而火狐、chrome文件名不带
+        String header = part.getHeader("content-disposition");
+        System.out.println("header : " + header);
+        String[] params = header.split(";");
+        String[] temp = params[2].split("=");
+        // 获取文件名，兼容各种浏览器的写法，去掉文件名前路径和双引号
+        String fileName = "";
+        if(temp[1].lastIndexOf("\\") < 0)
+            fileName = temp[1].substring(1, temp[1].length() - 1);
+        else
+            fileName = temp[1].substring(temp[1].lastIndexOf("\\") + 1, temp[1].length() - 1);
+        return fileName;
+    }
+}
